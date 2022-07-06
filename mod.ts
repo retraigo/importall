d3d81265d590b273c7b57868b28e4e92de2ebb48
Neko Of The Abyss <psi@innocent.com>
2022-07-06 17:32:51 +0530

    add other file support

--- mod.ts
+++ mod.ts
@@ -1,10 +1,16 @@
-export async function importAll(dir: string, extensions: (string|undefined)[] = ["ts", "js"]) {
+
+export async function importAll(
+  dir: string,
+  extensions: (string | undefined)[] = ["ts", "js"],
+) {
   const files = Array(...Deno.readDirSync(dir)).filter((x) =>
     x.isFile && (extensions.includes(x.name.split(".").at(-1)))
   );
   const promises = [];
   for (const file of files) {
-    promises.push(import(`${dir}/${file.name}`));
+    if (["ts", "js", "tsx", "jsx"].includes(x.name.split(".").at(-1))) {
+      promises.push(import(`${dir}/${file.name}`));
+    } else promises.push(Deno.readTextFile(`${dir}/${file.name}`));
   }
   const result = await Promise.all(promises);
   return result;
