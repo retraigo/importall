eb48b1b3722dd0bc030fea3b5252d3a7d53c9e14
Neko Of The Abyss <psi@innocent.com>
2022-07-06 17:39:47 +0530

    allow text files

--- mod.ts
+++ mod.ts
@@ -1,4 +1,3 @@
-
 export async function importAll(
   dir: string,
   extensions: (string | undefined)[] = ["ts", "js"],
@@ -8,10 +7,31 @@ export async function importAll(
   );
   const promises = [];
   for (const file of files) {
-    if (["ts", "js", "tsx", "jsx"].includes(x.name.split(".").at(-1))) {
+    if (
+      ["ts", "js", "tsx", "jsx"].includes(String(file.name.split(".").at(-1)))
+    ) {
       promises.push(import(`${dir}/${file.name}`));
     } else promises.push(Deno.readTextFile(`${dir}/${file.name}`));
   }
   const result = await Promise.all(promises);
   return result;
+}
+
+export async function importAllTextFiles(
+  dir: string,
+  extensions: (string | undefined)[] = ["ts", "js"],
+): Promise<Record<string, string>> {
+  const files = Array(...Deno.readDirSync(dir)).filter((x) =>
+    x.isFile && (extensions.includes(x.name.split(".").at(-1)))
+  );
+  const promises = [];
+  for (const file of files) {
+    promises.push(Deno.readTextFile(`${dir}/${file.name}`));
+  }
+  const result = await Promise.all(promises);
+  const res: Record<string, string> = {};
+  for(const i in files) {
+    res[files[i].name.split(".")[0]] = result[i];
+  }
+  return res;
 }
