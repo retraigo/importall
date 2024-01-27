083f025f652d256f4992e0676dc142efcdbcbed7
NeTT <nett@nett.moe>
2024-01-27 13:25:18 +0530

    remove importAllTextFiles and add readAllFiles, readAllTextFiles

--- mod.ts
+++ mod.ts
@@ -1,9 +1,9 @@
 export async function importAll(
   dir: string,
-  extensions: (string | undefined)[] = ["ts", "js"],
+  filter: (fileName: string) => boolean = (x) => x === "ts" || x === "js"
 ) {
-  const files = Array(...Deno.readDirSync(dir)).filter((x) =>
-    x.isFile && (extensions.includes(x.name.split(".").at(-1)))
+  const files = Array(...Deno.readDirSync(dir)).filter(
+    (x) => x.isFile && filter(x.name)
   );
   const promises = [];
   for (const file of files) {
@@ -17,12 +17,12 @@ export async function importAll(
   return result;
 }
 
-export async function importAllTextFiles(
+export async function readAllTextFiles(
   dir: string,
-  extensions: (string | undefined)[] = ["ts", "js"],
+  filter: (fileName: string) => boolean = (_) => true
 ): Promise<Record<string, string>> {
-  const files = Array(...Deno.readDirSync(dir)).filter((x) =>
-    x.isFile && (extensions.includes(x.name.split(".").at(-1)))
+  const files = Array(...Deno.readDirSync(dir)).filter(
+    (x) => x.isFile && filter(x.name)
   );
   const promises = [];
   for (const file of files) {
@@ -30,7 +30,26 @@ export async function importAllTextFiles(
   }
   const result = await Promise.all(promises);
   const res: Record<string, string> = {};
-  for(const i in files) {
+  for (const i in files) {
+    res[files[i].name.split(".")[0]] = result[i];
+  }
+  return res;
+}
+
+export async function readAllFiles(
+  dir: string,
+  filter: (fileName: string) => boolean = (_) => true
+): Promise<Record<string, Uint8Array>> {
+  const files = Array(...Deno.readDirSync(dir)).filter(
+    (x) => x.isFile && filter(x.name)
+  );
+  const promises = [];
+  for (const file of files) {
+    promises.push(Deno.readFile(`${dir}/${file.name}`));
+  }
+  const result = await Promise.all(promises);
+  const res: Record<string, Uint8Array> = {};
+  for (const i in files) {
     res[files[i].name.split(".")[0]] = result[i];
   }
   return res;
